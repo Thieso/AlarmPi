69be7ce13e37b4c9224aff64ab1d566bc7ccb5f1
Thieso <mail@thiesoelerich.com>
2020-03-18 21:12:43 +0100

    Finished setup of object oriented program. Now it needs testing ....

--- Alarm.py
+++ Alarm.py
@@ -1,14 +1,70 @@
+import time
 import pygame
 from luma.core.render import canvas
 from luma.core.legacy import text, show_message
 
 class Alarm:
     def __init__(self, virtual, device):
+        # initialize variables and set defaults
         self.hours = 6
         self.minutes = 30
         self.state = 0
+        self.blinkTime = 0.5
+        self.contrast = 10
         self.virtual = virtual
         self.device = device
+
+    def alarm_loop(self):
+        # initialze hour and time string
+        hour = 0
+        time_string = 0
+
+        while True:
+            # sleep for the blink time
+            time.sleep(blinkTime)
+
+            # set time string
+            new_time_string = time.strftime('%H%M')
+
+            # set alarm string for the right time
+            hour_string = str(self.hours).zfill(2)
+            minute_string = str(self.minutes).zfill(2)
+            alarm_string = hour_string + minute_string
+
+            # set contrast based on time
+            new_hour = int(time.strftime('%H'))
+            if hour != new_hour:
+                hour = new_hour
+                self.contrast = self.getContrast(hour)
+                self.device.contrast(self.contrast)
+
+            if time_string == alarm_string and self.state != 3:
+                self.state = 3
+                print("Sounding Alarm")
+                self.display_text(alarm_string)
+                self.sound_alarm()
+                # blink all the numbers during alarm sounding
+                # in order to not ring again wait till minute passed
+                while time.strftime('%H%M') == alarm_string:
+                    time.sleep(1)
+
+            # display information depending on the state
+            if self.state == 0:
+                time_string = new_time_string
+                # display time on display
+                self.display_text(time_string)
+            elif self.state == 1:
+                # blink the hours on the display
+                self.display_text(alarm_string)
+                time.sleep(self.blinkTime)
+                alarm_string = "--" + str(self.minutes).zfill(2)
+                self.display_text(alarm_string)
+            elif self.state == 2:
+                # blink the minutes on the display
+                self.display_text(alarm_string)
+                time.sleep(self.blinkTime)
+                alarm_string = str(self.hours).zfill(2) + "--"
+                self.display_text(alarm_string)
 
     def state_callback(self, channel):
         if self.state == 2 or self.state == 3:
@@ -63,7 +119,7 @@ class Alarm:
     def set_contrast(self, new_contrast):
         self.device.contrast(new_contrast)
 
-    def getContrast(self):
+    def getContrast(self, hour):
         b = 4.58e-3
         c = -2.19e-1
         d = 2.63
@@ -71,7 +127,7 @@ class Alarm:
         contrast = b*pow(self.hours,4) + c*pow(self.hours,3) + d*pow(self.hours,2) + f
         return min(int(contrast), 100)
 
-    def displayText(self, displayText):
+    def display_text(self, text):
         with canvas(self.virtual) as draw:
-            text(draw, (1, 0), displayText, fill="white")
+            text(draw, (1, 0), text, fill="white")
 
